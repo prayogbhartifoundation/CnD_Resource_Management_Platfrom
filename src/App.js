8f7a0c103a7d40a597179f1ab9124d0cd65bca1e
Adityaa27023 <130599871+Adityaa27023@users.noreply.github.com>
2025-01-13 20:52:46 +0530

    InventoryStatusPage_Change

--- src/App.js
+++ src/App.js
@@ -1,4 +1,5 @@
 import React from "react";
+import './App.css';
 import { BrowserRouter as Router, Route, Routes, Link } from "react-router-dom";
 import HomePage from "./pages/HomePage";
 import InventoryStatusPage from "./pages/InventoryStatusPage";
--- src/pages/InventoryStatusPage.js
+++ src/pages/InventoryStatusPage.js
@@ -4,56 +4,73 @@ import "../styles/InventoryStatusPage.css";
 function InventoryStatusPage() {
   return (
     <div className="inventory-status-page">
+      {/* Header */}
       <header className="inventory-header">
         <h1>Inventory Status</h1>
       </header>
 
+      {/* Main Content */}
       <main className="inventory-content">
-        {/* Table Section */}
-        <section className="table-section">
-          <table className="inventory-table">
-            <thead>
-              <tr>
-                <th>Agency Name</th>
-                <th>Agency Name</th>
-                <th>Agency Name</th>
-                <th>Agency Name</th>
-              </tr>
-            </thead>
-            <tbody>
-              <tr>
-                <td>
-                  650
-                  <br />
-                  <small>as of 4-1-25</small>
-                </td>
-                <td>
-                  650
-                  <br />
-                  <small>as of 4-1-25</small>
-                </td>
-                <td>--<br /><small>-</small></td>
-                <td>
-                  650
-                  <br />
-                  <small>as of 4-1-25</small>
-                </td>
-              </tr>
-            </tbody>
-          </table>
-        </section>
+        <div className="inventory-layout">
+          {/* Product List Section */}
+          <section className="product-list-container">
+            <h3 className="product-header">Products</h3>
+            {Array.from({ length: 10 }).map((_, index) => (
+              <div key={index} className="product-item">
+                <b>Product Name </b>
+               
+              </div>
+            ))}
+          </section>
 
-        {/* Product List */}
-        <aside className="product-list">
-          {Array.from({ length: 8 }).map((_, index) => (
-            <button key={index} className="product-button">Product Name</button>
-          ))}
-        </aside>
+          {/* Table Section */}
+          <section className="table-section">
+            <table className="inventory-table">
+              {/* Table Header */}
+              <thead>
+                <tr>
+                  <th>Agency Name</th>
+                  <th>Agency Name</th>
+                  <th>Agency Name</th>
+                  <th>Agency Name</th>
+                </tr>
+              </thead>
 
+              {/* Table Body */}
+              <tbody>
+                <tr>
+                  <td>
+                    650
+                    <br />
+                    <small>as of 4-1-25</small>
+                  </td>
+                  <td>
+                    650
+                    <br />
+                    <small>as of 4-1-25</small>
+                  </td>
+                  <td>
+                    --
+                    <br />
+                    <small>-</small>
+                  </td>
+                  <td>
+                    650
+                    <br />
+                    <small>as of 4-1-25</small>
+                  </td>
+                </tr>
+              </tbody>
+            </table>
+          </section>
+        </div>
+
+<container className="container-button">
         {/* Action Button */}
         <button className="query-button">
           Raise requirement query for Selected items
         </button>
+    </container>
 
         {/* Document Section */}
         <section className="document-section">
@@ -65,7 +82,7 @@ function InventoryStatusPage() {
           </div>
         </section>
       </main>
-    </div>
+      </div>
   );
 }
 
